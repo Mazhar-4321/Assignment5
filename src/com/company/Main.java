a4a22cd1c0e6d2bc913c27967757c8e55bf870ba
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 19:05:10 +0530

    ADDED: Code to find percentage of head and tails of N flip coins

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    final int OFFSET_VALUE = 1;
 
     public static void main(String[] args) {
         System.out.println("Basic Core Java Programs");
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        int flipNumber = scanner.nextInt();
+        double[] headAndTailPercentage = main.findPercentageOfHeadAndTailsOfNFlipCoins(flipNumber);
+        System.out.println("Head Percentage=" + headAndTailPercentage[0] + ",Tail Percentage=" + headAndTailPercentage[1]);
+    }
+
+    public double[] findPercentageOfHeadAndTailsOfNFlipCoins(int flipNumber) {
+        double[] headAndTailPercentage = new double[2];
+        int count = flipNumber;
+        while (flipNumber-- >= OFFSET_VALUE) {
+            double randomNumber = Math.random();
+            headAndTailPercentage[randomNumber > 0.5 ? 0 : 1] += 1;
+        }
+        headAndTailPercentage[0] = headAndTailPercentage[0] / count;
+        headAndTailPercentage[1] = headAndTailPercentage[1] / count;
+        return headAndTailPercentage;
     }
 }
