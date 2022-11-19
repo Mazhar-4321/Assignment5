cf2f53cfc5d542aab586eccfe8bab4fbb4a01db5
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-19 19:39:48 +0530

    ADDED: Code to swap two numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,30 +3,43 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    final static int Q1 = 1;
+    final static int Q3 = 3;
+    final static int Q7 = 7;
     final int OFFSET_VALUE = 1;
     final int POWER_LIMIT = 30;
 
     public static void main(String[] args) {
         System.out.println("Basic Core Java Programs");
         Main main = new Main();
+        System.out.println("Enter 1 for Flip Coin ,3 for Power of 2 ,7 for swap two numbers  ");
         Scanner scanner = new Scanner(System.in);
-        int flipNumber = scanner.nextInt();
-        double[] headAndTailPercentage = main.findPercentageOfHeadAndTailsOfNFlipCoins(flipNumber);
-        System.out.println("Head Percentage=" + headAndTailPercentage[0] + ",Tail Percentage=" + headAndTailPercentage[1]);
-        int N = scanner.nextInt();
-        main.printPowerOf2(N);
+        int option = scanner.nextInt();
+        switch (option) {
+            case Q1:
+                int flipNumber = scanner.nextInt();
+                main.findPercentageOfHeadAndTailsOfNFlipCoins(flipNumber);
+                break;
+            case Q3:
+                int N = scanner.nextInt();
+                main.printPowerOf2(N);
+                break;
+            case Q7:
+                int num1 = scanner.nextInt();
+                int num2 = scanner.nextInt();
+                main.swapTwoNumbers(num1, num2);
+                break;
+        }
     }
 
-    public double[] findPercentageOfHeadAndTailsOfNFlipCoins(int flipNumber) {
+    public void findPercentageOfHeadAndTailsOfNFlipCoins(int flipNumber) {
         double[] headAndTailPercentage = new double[2];
         int count = flipNumber;
         while (flipNumber-- >= OFFSET_VALUE) {
             double randomNumber = Math.random();
             headAndTailPercentage[randomNumber > 0.5 ? 0 : 1] += 1;
         }
-        headAndTailPercentage[0] = headAndTailPercentage[0] / count;
-        headAndTailPercentage[1] = headAndTailPercentage[1] / count;
-        return headAndTailPercentage;
+        System.out.println("Head Percentage=" + headAndTailPercentage[0] / count + ",Tail Percentage=" + headAndTailPercentage[1] / count);
     }
 
     public void printPowerOf2(int N) {
@@ -37,5 +50,13 @@ public class Main {
         while (i++ != N) {
             System.out.println((int) Math.pow(2, i));
         }
+    }
+
+    public void swapTwoNumbers(int num1, int num2) {
+        System.out.println(" Before Swap num1=" + num1 + ",num2=" + num2);
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("After Swap num1=" + num1 + ",num2=" + num2);
     }
 }
